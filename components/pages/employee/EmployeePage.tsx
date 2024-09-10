050a9d548148f01a3c3e502942b1272762143353
Juliya Boiko <boikoj@ou.org>
2024-09-10 16:57:48 +0300

    add employees projects

--- components/pages/employee/EmployeePage.tsx
+++ components/pages/employee/EmployeePage.tsx
@@ -3,20 +3,24 @@ import { EmployeeInfo } from '@/components/sections/employee/info/EmployeeInfo';
 import styles from './employeePage.module.scss';
 import { Topping } from '@/components/topping/Topping';
 import { IDynamicComponent } from '@/typings';
-import { useEmployee, useCompany } from '@/utils';
+import { useEmployee, useCompany, useProjects } from '@/utils';
+import { ProjectsList } from '@/components/sections/dashboard/projects/progectsList/ProjectsList';
 
 export const EmployeePage = ({ slug }: IDynamicComponent) => {
   const { data: user, isLoading } = useEmployee(slug);
   const { data: company } = useCompany();
+  const { data: projects, isLoading: isProjectsLoading } = useProjects();
 
-  // console.log({ isLoading, user });
+  const employeeProjects = projects
+    ? projects.filter((project) => project.tasks.assignee.some((el) => el._id === slug))
+    : undefined;
 
   return (
     <div className={styles.employeePage}>
       <Topping title="Employee’s Profile"></Topping>
       <div className={styles.container}>
         <EmployeeInfo company={company} user={user} loading={isLoading} />
-        <div>table</div>
+        <ProjectsList loading={isProjectsLoading} projects={employeeProjects} />
       </div>
     </div>
   );
--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,3 @@
-import { jwtDecode } from 'jwt-decode';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
@@ -6,23 +5,7 @@ export async function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
   const isPublicPath = path === '/sign-in' || path === '/sign-up';
   const token = request.cookies.get('workroom')?.value || '';
-  let isValid = false;
-  if (token) {
-    try {
-      const decodedToken = jwtDecode(token);
-      const { exp } = decodedToken as { exp: number };
-      if (exp && Date.now() < exp * 1000) {
-        isValid = true;
-      }
-    } catch (error) {
-      console.log(error);
-      isValid = false;
-    }
-  }
 
-  if (!isValid) {
-    return NextResponse.redirect(new URL('/sign-in', request.nextUrl));
-  }
   if (isPublicPath && token) {
     return NextResponse.redirect(new URL('/', request.nextUrl));
   }
