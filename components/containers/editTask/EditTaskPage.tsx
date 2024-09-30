a473fc35eebcf5e3fbaf897e06210ab425bbb1c8
Juliya Boiko <boikoj@ou.org>
2024-09-30 20:37:17 +0300

    update edit task page

--- components/containers/editTask/EditTaskPage.tsx
+++ components/containers/editTask/EditTaskPage.tsx
@@ -5,15 +5,14 @@ import { useTask } from '@/services';
 import { IDynamicComponent } from '@/typings';
 import { Topping } from '@/components/topping/Topping';
 import { Preloader, TaskStatusDrop } from '@/components/ui';
-import { EditTaskForm } from '@/components/forms/editTask/EditTaskForm';
 
 export const EditTaskPage = ({ slug }: IDynamicComponent) => {
-  const { data, isLoading: isLoadingTask } = useTask(slug);
+  const { data: task, isLoading: isLoadingTask } = useTask(slug);
 
   return (
     <div className={styles.editTaskPage}>
-      <Topping link="Back to task" path={`${ROUTES.task}/${data?.task?._id}`} title="Edit task">
-        {data && <TaskStatusDrop id={data.task._id} status={data.task.status} />}
+      <Topping link="Back to task" path={`${ROUTES.task}/${task?._id}`} title="Edit task">
+        {task && <TaskStatusDrop id={task._id} status={task.status} />}
       </Topping>
       <div className={styles.container}>
         {isLoadingTask && (
@@ -21,7 +20,6 @@ export const EditTaskPage = ({ slug }: IDynamicComponent) => {
             <Preloader />
           </div>
         )}
-        {data && <EditTaskForm task={data.task} attachments={data.attachments} />}
       </div>
     </div>
   );
