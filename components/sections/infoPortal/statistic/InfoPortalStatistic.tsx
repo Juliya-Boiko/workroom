48fe0fd6a836ced618abdf637c7497556c6a667f
Juliya Boiko <boikoj@ou.org>
2024-10-30 11:44:49 +0200

    add statistic & chart

--- components/sections/infoPortal/statistic/InfoPortalStatistic.tsx
+++ components/sections/infoPortal/statistic/InfoPortalStatistic.tsx
@@ -1,24 +1,36 @@
 'use client';
 import styles from './infoPortalStatistic.module.scss';
 import { useProjects } from '@/services';
-import { growthAmount } from '@/utils';
+import { growthAmount, lastAmount, getDynamic } from '@/utils';
+import { LoaderSkeleton } from '@/components/LoaderSkeleton';
+import { LineChart, Line } from 'recharts';
 
 export const InfoPortalStatistic = () => {
   const { data, isLoading } = useProjects(null);
 
-  // console.log({ data });
-
   return (
     <section className={styles.infoPortalStatistic}>
       <h2 className={styles.title}>Current Projects</h2>
-      <div className={styles.wrapper}>
-        <div>
-          <div className={styles.amount}>{data ? data.total : ''}</div>
-          {data && data.total ? <div className={styles.dynamic}>Growth +{growthAmount(data)}</div> : null}
+      {isLoading ? <LoaderSkeleton height={54} /> : null}
+      {data ? (
+        <div className={styles.wrapper}>
+          <div>
+            <div className={styles.amount}>{data.total}</div>
+            {data.total ? (
+              <div className={styles.dynamic}>Growth +{growthAmount(data.projects)}</div>
+            ) : null}
+          </div>
+          <div className={styles.chart}>
+            <LineChart width={100} height={80} data={getDynamic(data.projects)}>
+              <Line type="monotone" dataKey="pv" stroke="#3F8CFF" strokeWidth={2} />
+            </LineChart>
+          </div>
         </div>
-        <div>line</div>
-      </div>
-      <p className={styles.subtitle}>Ongoing projects last month - 7</p>
+      ) : null}
+      <p className={styles.subtitle}>
+        {isLoading ? <LoaderSkeleton height={14} /> : null}
+        {data ? `Ongoing projects last month - ${lastAmount(data.projects)}` : null}
+      </p>
     </section>
   );
 };
--- utils/helpers/statisticCounters.ts
+++ utils/helpers/statisticCounters.ts
@@ -1,10 +1,64 @@
 import { IProject } from '@/typings';
 
 export const growthAmount = (data: IProject[]) => {
-  if (!data || (data && !data.projects.length)) return 0;
-  const currentMonth = new Date();
+  const currentYear = new Date().getFullYear();
+  const currentMonth = new Date().getMonth();
+  const count = data.filter((item) => {
+    const date = new Date(item.createdAt);
+    return date.getFullYear() === currentYear && date.getMonth() === currentMonth;
+  }).length;
+  return count;
+};
 
-  console.log({ currentMonth, data });
+export const lastAmount = (data: IProject[]) => {
+  const now = new Date();
+  const lastMonth = new Date(now.getFullYear(), now.getMonth() - 1);
+  const count = data.filter((item) => {
+    const date = new Date(item.createdAt);
+    return (
+      date.getFullYear() === lastMonth.getFullYear() && date.getMonth() === lastMonth.getMonth()
+    );
+  }).length;
+  return count;
+};
 
-  return 444;
+interface MonthCount {
+  [key: string]: { month: string; pv: number };
+}
+
+export const getDynamic = (data: IProject[]) => {
+  const now = new Date();
+
+  const months = [
+    new Date(now.getFullYear(), now.getMonth() - 2),
+    new Date(now.getFullYear(), now.getMonth() - 1),
+    new Date(now.getFullYear(), now.getMonth()),
+  ];
+
+  const monthMap: MonthCount = months.reduce<MonthCount>((acc, date) => {
+    const yearMonth = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}`;
+    acc[yearMonth] = { month: yearMonth, pv: 0 };
+    return acc;
+  }, {});
+
+  const endOfCurrentMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0, 23, 59, 59, 999);
+
+  const filteredItems = data.filter((item) => {
+    const date = new Date(item.createdAt);
+    return date >= months[0] && date <= endOfCurrentMonth;
+  });
+
+  filteredItems.forEach((item) => {
+    const date = new Date(item.createdAt);
+    const yearMonth = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}`;
+    if (monthMap[yearMonth]) {
+      monthMap[yearMonth].pv += 1;
+    }
+  });
+
+  const sortedArray = Object.values(monthMap)
+    .sort((a, b) => new Date(a.month).getTime() - new Date(b.month).getTime())
+    .map(({ pv }) => ({ amt: 1, pv }));
+
+  return sortedArray;
 };
